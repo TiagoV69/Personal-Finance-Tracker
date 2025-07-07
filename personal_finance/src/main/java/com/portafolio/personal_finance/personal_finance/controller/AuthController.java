053fd7a9b3e15a0b4d6ff17a3a9912b8d50f418a
TiagoV69 <santyvarela.03@gmail.com>
2025-07-07 12:24:50 -0500

    feat(auth): Crea AuthController con endpoint público para registro de usuarios

---
+++ personal_finance/src/main/java/com/portafolio/personal_finance/personal_finance/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.portafolio.personal_finance.personal_finance.controller;
+
+
+import com.portafolio.personal_finance.personal_finance.dto.RegisterRequest;
+import com.portafolio.personal_finance.personal_finance.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth") // Ruta base para este controlador
+@RequiredArgsConstructor
+public class AuthController {
+
+    // inyectamos nuestro servicio de logica de negocio
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+
+        // Delegamos toda la logica de registro al servicio
+        authService.register(request);
+
+        // Devolvemos una respuesta HTTP 200 OK con un mensaje de éxito.
+        return ResponseEntity.ok("Usuario registrado exitosamente");
+    }
+}
