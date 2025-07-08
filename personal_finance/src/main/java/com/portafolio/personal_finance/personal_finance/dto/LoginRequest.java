9c39662431b4a2569cf503df968e485f7afd64fa
TiagoV69 <santyvarela.03@gmail.com>
2025-07-07 20:32:41 -0500

    feat(auth): Añado dependencias de JJWT y DTOs para el flujo del login

---
+++ personal_finance/src/main/java/com/portafolio/personal_finance/personal_finance/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.portafolio.personal_finance.personal_finance.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
